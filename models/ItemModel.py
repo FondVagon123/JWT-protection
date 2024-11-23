b98eaf36c3e2f621d5de3d822d27aa6a827aa162
Kiril Kirichenko <153592801+FondVagon123@users.noreply.github.com>
2024-11-23 14:03:50 +0200

    Create ItemModel.py

---
+++ models/ItemModel.py
@@ -0,0 +1,8 @@
+from db import db
+
+class ItemModel(db.Model):
+    __tablename__ = "items"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    price = db.Column(db.Float, nullable=False)
